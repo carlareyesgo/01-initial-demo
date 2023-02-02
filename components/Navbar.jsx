84d29c7f71789e22aede82d9715dd0c12e19ee90
Carla Reyes <carlareyesgo@gmail.com>
2023-02-02 12:56:44 -0700

    Custom ActiveLink

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
 import React from 'react'
 import styles from './Navbar.module.css'
+import { ActiveLink } from './ActiveLink'
 
 export const Navbar = () => {
     return (
         <nav className={styles['menu-container']}>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/">Contact</Link>
+            <ActiveLink text="Home" href="/"/>
+            <ActiveLink text="About" href="/about"/>
+            <ActiveLink text="Contact" href="/contact"/>
         </nav>
     )
 }
