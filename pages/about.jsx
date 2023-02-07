318313acad31658c08519f5bcf4157298592ed7e
Carla Reyes <carlareyesgo@gmail.com>
2023-02-07 14:15:24 -0700

    Multiples Layouts anidados

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,25 +1,37 @@
-import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Link from 'next/link'
-import {Navbar} from '../components/Navbar'
 import { MainLayout } from '../components/layouts/MainLayout'
+import { DarkLayout } from '../components/layouts/DarkLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function AboutPage() {
-  return (
-   <MainLayout>
-     <h1>About Page</h1>
+ return (
+    <>
+      
+        <h1>About Page</h1>
         <h1 className={'title'}>
-         {/*  Ir a <a href="/">Home</a> */}
-         Ir a <Link href="/">Home</Link>
+          {/*  Ir a <a href="/">Home</a> */}
+          Ir a <Link href="/">Home</Link>
         </h1>
         <div className={'description'}>
           <p>
-            Get started by editing&nbsp;
+            Get started by editing bye
             <code className={'code'}>pages/about.jsx</code>
           </p>
         </div>
-   </MainLayout>
+      
+    </>
+  )
+}
+
+AboutPage.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>
+        {page}
+      </DarkLayout>
+    </MainLayout>
+
   )
 }
